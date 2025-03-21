cbabed181634493630a48c4ddaf7ea84625feb4b
Bryan <bryankurniaakbarr12@gmail.com>
2025-03-21 21:58:42 +0700

    baru vite

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
     server: {
-        origin: 'https://course-online-production.up.railway.app', // Pakai HTTPS
-    }
+        origin: "https://course-online-production.up.railway.app", // Pakai HTTPS
+    },
+    build: {
+        outDir: "public/build", // Pastikan hasil build ke folder publik
+        emptyOutDir: true,
+        manifest: true,
+    },
 });
